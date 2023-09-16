f890845c31568a82410b2b9bcdbe63a7ed41c3b0
msojocs <jiyecafe@gmail.com>
2023-09-16 23:00:47 +0800

    feat: v4.6.1 in progress

--- ildasm.sh
+++ ildasm.sh
@@ -8,6 +8,7 @@ dll_file_webui="$root_dir/original/Fiddler.WebUi.dll"
 ildasm -tok -byt $dll_file_webui -out=Fiddler.WebUi.il
 # ilasm -dll -X64 -output=$dll_file_webui Fiddler.WebUi.il
 # ilasm -dll -X64 -output=Fiddler.WebUi.dll Fiddler.WebUi.il
+# ildasm /tok /byt Fiddler.WebUi.dll /out=Fiddler.WebUi.il
 
 dll_file_sdk="$root_dir/original/FiddlerBackendSDK.dll"
 ildasm -tok -byt $dll_file_sdk -out=FiddlerBackendSDK.il
