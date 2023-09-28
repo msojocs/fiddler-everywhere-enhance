6d886a6ba7ae501ba46417f2e93671725f1ab0ef
msojocs <jiyecafe@gmail.com>
2023-09-28 23:13:30 +0800

    perf: 优化体验

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@
   const fs = require('fs')
   http.createServer((req, res) => {
     res.setHeader('Signature', 'SignedHeaders=content-type, Signature=AAAAWzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABNsAzGwa7Q3iTZFqv3xYHemw/qxkwk0sIC/usJVi7713VJv0B1JbfuiDXxHfScNyyQjkuaHKtwbn5qUeHjFwpGbEwT7g2t3hdiBTpJ+406wmIST7bK+eY/HU283penaNN9dDWv/ndsvDHCEcckxvSb7XwFBcdy0/Nq3RC9FKAPug')
-    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    
     const fullPath = req.url
     const url = new URL(fullPath, 'http://127.0.0.1:5678')
     console.log(url.pathname)
@@ -12,6 +12,7 @@
     if (url != null) {
       const loc = path.resolve(__dirname, `./file/${url.pathname}`)
       if (fs.existsSync(loc + '.json')) { // 在后面加上.json后缀，存在就用这个
+        res.setHeader('Content-Type', 'application/json; charset=utf-8')
         data = fs.readFileSync(loc + '.json').toString()
       }
       else if (fs.existsSync(loc)) { // 直接使用原始路径
