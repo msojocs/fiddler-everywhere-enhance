6766351266d09163dc108082d65b4bfd1a40946f
msojocs <jiyecafe@gmail.com>
2025-08-21 09:13:34 +0800

    perf: init for test

--- tool/fe/fe_test.go
+++ tool/fe/fe_test.go
@@ -1,11 +1,32 @@
 package fe_test
 
 import (
+	"errors"
 	"fe-tool/fe"
 	"log"
+	"os"
 	"testing"
 )
 
+func init() {
+	// 0.准备文件夹
+	log.Println("check cache dir.")
+	_, err := os.Stat("cache")
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			// 不存在
+			err := os.Mkdir("cache", 0755)
+			if err != nil {
+				log.Fatalln("Create dir error:", err)
+			} else {
+				log.Println("Create dir ok.")
+			}
+		} else {
+			log.Fatalln("Check cache dir error:", err)
+		}
+	}
+
+}
 func TestDownload(t *testing.T) {
 	fe.Download()
 }
--- tool/server/server_test.go
+++ tool/server/server_test.go
@@ -1,10 +1,32 @@
 package server_test
 
 import (
+	"errors"
 	"fe-tool/server"
 	"log"
+	"os"
 	"testing"
 )
+
+func init() {
+	// 0.准备文件夹
+	log.Println("check cache dir.")
+	_, err := os.Stat("cache")
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			// 不存在
+			err := os.Mkdir("cache", 0755)
+			if err != nil {
+				log.Fatalln("Create dir error:", err)
+			} else {
+				log.Println("Create dir ok.")
+			}
+		} else {
+			log.Fatalln("Check cache dir error:", err)
+		}
+	}
+
+}
 
 func TestDownload(t *testing.T) {
 	server.Download()
